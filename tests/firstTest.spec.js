a10ec7d5413b40043307f6a1a937b88a44b7b6f2
lihka1202 <mnakhilwork@gmail.com>
2024-01-08 16:30:57 +0800

    feat(test) : child elements finder system located

--- tests/firstTest.spec.js
+++ tests/firstTest.spec.js
@@ -5,3 +5,18 @@ test.beforeEach(async ({ page }) => {
   await page.getByText('Forms').click();
   await page.getByText('Form Layout').click();
 });
+
+test('user facing locators', async ({ page }) => {
+  await page.getByRole('textbox', { name: 'Email' }).first().click();
+  await page.getByRole('button', { name: 'Sign in' }).first().click();
+  await page.getByLabel('Email').first().click();
+  await page.getByPlaceholder('Jane Doe').first().click();
+  await page.getByText('Using the Grid').first().click();
+  await page.getByTitle('IoT Dashboard').click();
+});
+
+test('Child Element Locator', async ({ page }) => {
+  await page.locator('nb-card nb-radio :text-is("Option 1")');
+  await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 1")');
+  await page.locator('nb-card').getByRole('button', { name: 'Sign in' });
+});
